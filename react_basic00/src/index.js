d224e94dd734c898b453fa06cab6c4e94602f6a1
IMMJ <story_mania@naver.com>
2022-08-23 09:59:45 +0900

    ?

--- react_basic00/src/index.js
+++ react_basic00/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'
+import MainVisual from './MainVisual';
+import Loading from './Loading'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
 );
+
